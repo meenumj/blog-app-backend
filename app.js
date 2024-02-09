e2b5394902eecf0c5dbf224fac823e19de6db4b0
meenumj <meenu167999@gmail.com>
2024-02-09 10:14:32 +0530

    API for Signup

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors= require("cors")
+const userRouter=require("./Controllers/UserRouter")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+app.use("/api/blog",userRouter)
+
+mongoose.connect("mongodb+srv://meenumj:meenumj167@cluster0.uobnjw6.mongodb.net/blogDb?retryWrites=true&w=majority",
+ {useNewUrlParser:true})
+
+app.listen(3001,()=>{
+    console.log("Server Running")
+})
